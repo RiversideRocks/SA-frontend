d94ef9673afe016c8205f6bb131fce6427a9fdde
RiversideRocks <wilest7@gmail.com>
2021-03-13 13:14:58 -0500

    Beta 1.0

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+
+Requires:
+
+jquery
+js-SHA256
+
+*/
+
+
+$.get("https://checkip.amazonaws.com/", function(data, status){
+    console.log("Status: Was IP address collection ok? "+status)
+    var hash = sha256(data)
+    $.get(`https://ipapi.co/${data}country/`, function(data, status){
+        console.log("Status: Was country collection ok? "+status)
+        var country = data
+        var ogdate = new Date().valueOf();
+        var unrounded_epoch = ogdate / 1000;
+        var epoch = Math.round(unrounded_epoch)
+        $.post("https://riverside.rocks/apis/inbound.php?d="+window.location.hostname, {epoch: epoch, country: country, token: hash});
+    })
+})
